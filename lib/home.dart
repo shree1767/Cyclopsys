8e5265455e77b1591e9f81dea779e2246ec2389a
shree1767 <shreenidhi1757@gmail.com>
2024-08-10 22:27:01 +0530

    project init

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home Page',
+            ),
+          ],
+        ),),
+      );
+  }
+}
